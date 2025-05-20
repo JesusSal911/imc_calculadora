024379d688b5dbbaa00546bd149627b66451978d
Jesús Salvador <salvadoralexander7523@gmail.com>
2025-05-19 20:43:35 -0500

    feat(results): implementar pantalla de resultados con: - Cálculo IMC (peso/altura²) - Visualización con estilos condicionales - Categorización por rangos - Botón de retroceso

--- lib/screens/imc_result_screen.dart
+++ lib/screens/imc_result_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculadora/core/app_colors.dart';
+import 'package:imc_calculadora/core/text_styles.dart';
 
 class ImcResultScreen extends StatelessWidget {
   final int weight;
@@ -15,6 +17,90 @@ class ImcResultScreen extends StatelessWidget {
       backgroundColor: AppColors.background,
       appBar: toolbarResult(),
       body: bodyResult(context),
+    );
+  }
+
+  Padding bodyResult(BuildContext context) {
+    double fixedHeight = height / 100; // Convertir altura a metros
+    double imcResult = weight / (fixedHeight * fixedHeight);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Tu resultado",
+            style: TextStyle(
+              fontSize: 38,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 32, bottom: 32),
+              child: Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: AppColors.backgroundComponent,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text(
+                      getTitleByImc(imcResult),
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.w600,
+                        color: getColorByImc(imcResult),
+                      ),
+                    ),
+                    Text(
+                      imcResult.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontSize: 76,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(18.0),
+                      child: Text(
+                        getDescriptionByImc(imcResult),
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.w200,
+                          color: Colors.white,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: ButtonStyle(
+                shape: WidgetStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                backgroundColor: WidgetStateProperty.all(AppColors.primary),
+              ),
+              child: Text("Finalizar", style: TextStyles.bodyText),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
