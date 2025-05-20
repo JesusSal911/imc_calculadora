9603279ff2fc760c01820c09011acb24a7bdf1d5
Jesús Salvador <salvadoralexander7523@gmail.com>
2025-05-19 20:38:22 -0500

    feat(calculator): implementar pantalla de resultados IMC con datos de entrada

---
+++ lib/screens/imc_result_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ImcResultScreen extends StatelessWidget {
+  final int weight;
+  final double height;
+  const ImcResultScreen({
+    super.key,
+    required this.weight,
+    required this.height,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
