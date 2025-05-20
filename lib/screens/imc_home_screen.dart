69f14656a2682deb4f26eb16349d3317e74b0460
Jesús Salvador <salvadoralexander7523@gmail.com>
2025-05-19 19:44:38 -0500

    chore(project): inicializamos estructura de Flutter creando componentes, agregando una paleta de colores y las importaciones correspondientes

---
+++ lib/screens/imc_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImcHomeScreen extends StatefulWidget {
+  const ImcHomeScreen({super.key});
+
+  @override
+  State<ImcHomeScreen> createState() => _ImcHomeScreenState();
+}
+
+class _ImcHomeScreenState extends State<ImcHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
